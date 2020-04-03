372ed63ec12898a037ac879d964a4820498733af
Md Akhtar <M1049199@mindtree.com>
2020-04-04 02:30:28 +0530

    data feeding done

--- ngoList/ngoTable/models.py
+++ ngoList/ngoTable/models.py
@@ -3,10 +3,14 @@ from django.db import models
 # NGO Table model
 
 class NgoTable(models.Model):
-    LOCATION = models.CharField(max_length=200)
+    STATE_UT = models.CharField(max_length=200)
+    SPECIFIC_LOCATION = models.CharField(max_length=200)
     TARGET_GROUPS = models.CharField(max_length=200)
-    ORGANIZATION = models.CharField(max_length=200)
     TYPE_OF_BENEFIT = models.CharField(max_length=200)
-    WHAT_WILL_BE_PROVIDED = models.CharField(max_length=200)
-    DONATION_LINKS_INFO= models.CharField(max_length=200)
-    Exemptions = models.CharField(max_length=200)
+    ORGANIZATION = models.CharField(max_length=200)
+    INITIATIVE = models.CharField(max_length=200)
+    WEBSITE = models.CharField(max_length=200)
+    DONATION_LINK = models.CharField(max_length=200)
+    DONATION_INFO= models.CharField(max_length=200)
+    FOREIGN_DONATION = models.CharField(max_length=200)
+    INFO = models.CharField(max_length=200)
